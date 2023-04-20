4218d9ea3806d9f752f8c6282381135919ce06f2
Nikita Nikitin <nikitin.ninise@gmail.com>
2023-04-20 06:52:09 -0400

    Shitty Google doesn't work normally with a jetpack compose

--- app/src/main/java/com/ndteam/wasteandroidapp/view/main/screens/search/NativeAdItemView.kt
+++ app/src/main/java/com/ndteam/wasteandroidapp/view/main/screens/search/NativeAdItemView.kt
@@ -1,5 +1,7 @@
 package com.ndteam.wasteandroidapp.view.main.screens.search
 
+import android.os.Bundle
+import android.view.View
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -16,6 +18,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -24,7 +28,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import coil.compose.AsyncImage
+import com.appnexus.opensdk.NativeAdEventListener
+import com.appnexus.opensdk.NativeAdSDK
 import com.google.android.gms.ads.nativead.NativeAd
 import com.ndteam.wasteandroidapp.ui.theme.*
 
@@ -32,94 +39,143 @@ import com.ndteam.wasteandroidapp.ui.theme.*
 @Composable
 fun NativeAdItemView(ad: NativeAd? = null) {
 
-    Box {
-        Column (modifier = Modifier
-            .fillMaxWidth()
-            .background(color = Color.White)
-            .padding(vertical = 10.dp)) {
-            Row (modifier = Modifier.padding(vertical = 5.dp)) {
-                AsyncImage(
-                    model = ad?.images?.first()?.uri,
-                    contentDescription = "Ads logo",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(50.dp, height = 50.dp)
-                        .background(color = Color.Black)
+    val composeView = ComposeView(LocalContext.current)
+    val composeViewClickThrough: ComposeView = ComposeView(LocalContext.current)
 
-                )
+    AndroidView(factory = {
+        composeView.apply {
+            setContent {
+                Box {
+                    Column (modifier = Modifier
+                        .fillMaxWidth()
+                        .background(color = Color.White)
+                        .padding(vertical = 10.dp)) {
+                        Row (modifier = Modifier.padding(vertical = 5.dp)) {
+                            AsyncImage(
+                                model = ad?.images?.first()?.uri,
+                                contentDescription = "Ads logo",
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .size(50.dp, height = 50.dp)
+                                    .background(color = Color.Black)
 
-                Spacer(modifier = Modifier.width(10.dp))
+                            )
 
-                Column (horizontalAlignment = Alignment.Start) {
-                    ad?.headline?.let {
+                            Spacer(modifier = Modifier.width(10.dp))
+
+                            Column (horizontalAlignment = Alignment.Start) {
+                                ad?.headline?.let {
+                                    Text(
+                                        text = it,
+                                        color = TitleText,
+                                        fontFamily = OpenSans,
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = 16.sp,
+                                        letterSpacing = 1.sp
+
+                                    )
+                                }
+
+                                ClickableText(
+                                    text = AnnotatedString("Open"),
+                                    style = TextStyle(
+                                        color = Color.Blue,
+                                        fontFamily = FontFamily.SansSerif,
+                                        fontWeight = FontWeight.Normal,
+                                        fontSize = 14.sp,
+                                        letterSpacing = 1.sp
+                                    ),
+                                    onClick = {
+                                        ad?.performClick(Bundle())
+                                    })
+                            }
+
+                        }
+
+                        ad?.body?.let {
+                            Text(
+                                text = it,
+                                color = TitleText,
+                                fontFamily = OpenSans,
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 12.sp,
+                                letterSpacing = 1.sp,
+                                modifier = Modifier.padding(bottom = 2.dp)
+                            )
+                        }
+                    }
+
+                    Box(
+                        modifier = Modifier
+                            .width(30.dp)
+                            .height(30.dp)
+                            .padding(4.dp)
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(Color.White)
+                            .align(alignment = Alignment.TopEnd)
+                            .border(
+                                1.dp,
+                                Color.Gray,
+                                shape = RoundedCornerShape(8.dp),
+                            )
+
+
+                    ) {
                         Text(
-                            text = it,
+                            text = "Ad",
                             color = TitleText,
                             fontFamily = OpenSans,
                             fontWeight = FontWeight.Medium,
-                            fontSize = 16.sp,
-                            letterSpacing = 1.sp
-
+                            fontSize = 10.sp,
+                            letterSpacing = 1.sp,
+                            modifier = Modifier.align(alignment = Alignment.Center)
                         )
                     }
 
-                    ClickableText(
-                        text = AnnotatedString("Open"),
-                        style = TextStyle(
-                            color = Color.Blue,
-                            fontFamily = FontFamily.SansSerif,
-                            fontWeight = FontWeight.Normal,
-                            fontSize = 14.sp,
-                            letterSpacing = 1.sp
-                        ),
-                        onClick = {
+                    AndroidView(factory = {
+                        composeViewClickThrough.apply { }
+                    })
 
-                        })
                 }
-
             }
 
-            ad?.body?.let {
-                Text(
-                    text = it,
-                    color = TitleText,
-                    fontFamily = OpenSans,
-                    fontWeight = FontWeight.Normal,
-                    fontSize = 12.sp,
-                    letterSpacing = 1.sp,
-                    modifier = Modifier.padding(bottom = 2.dp)
-                )
+        }
+    })
+
+
+    NativeAdSDK.unRegisterTracking(composeView)
+    NativeAdSDK.registerTracking(
+        nativeAdResponse,
+        composeView,
+        mutableListOf(composeViewClickThrough) as List<View>?,
+        object : NativeAdEventListener {
+            override fun onAdWasClicked() {
+                TODO("Not yet implemented")
             }
+
+            override fun onAdWasClicked(p0: String?, p1: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onAdWillLeaveApplication() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onAdImpression() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onAdAboutToExpire() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onAdExpired() {
+                TODO("Not yet implemented")
+            }
+
         }
+    )
 
-        Box(
-            modifier = Modifier
-                .width(30.dp)
-                .height(30.dp)
-                .padding(4.dp)
-                .clip(RoundedCornerShape(8.dp))
-                .background(Color.White)
-                .align(alignment = Alignment.TopEnd)
-                .border(
-                    1.dp,
-                    Color.Gray,
-                    shape = RoundedCornerShape(8.dp),
-                )
-
-
-        ) {
-            Text(
-                text = "Ad",
-                color = TitleText,
-                fontFamily = OpenSans,
-                fontWeight = FontWeight.Medium,
-                fontSize = 10.sp,
-                letterSpacing = 1.sp,
-                modifier = Modifier.align(alignment = Alignment.Center)
-            )
-        }
-
-    }
 
 
 }
