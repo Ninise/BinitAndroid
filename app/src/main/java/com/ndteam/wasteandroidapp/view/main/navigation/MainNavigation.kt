ea27c429ab4b097216dc29beb48bc717a705a7cf
Nikita Nikitin <nikitin.ninise@gmail.com>
2023-05-12 06:50:09 -0400

    Fixed issue with blank screen on closing a GameScreen

--- app/src/main/java/com/ndteam/wasteandroidapp/view/main/navigation/MainNavigation.kt
+++ app/src/main/java/com/ndteam/wasteandroidapp/view/main/navigation/MainNavigation.kt
@@ -1,9 +1,12 @@
 package com.ndteam.wasteandroidapp.view.main.navigation
 
+import android.content.Intent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
+import androidx.navigation.Navigation
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -11,6 +14,7 @@ import androidx.navigation.navArgument
 import com.ndteam.wasteandroidapp.models.RecycleType
 import com.ndteam.wasteandroidapp.utils.Const
 import com.ndteam.wasteandroidapp.utils.Utils
+import com.ndteam.wasteandroidapp.view.game.GameActivity
 import com.ndteam.wasteandroidapp.view.main.MainViewModel
 import com.ndteam.wasteandroidapp.view.main.screens.main.MainScreen
 import com.ndteam.wasteandroidapp.view.main.screens.search.GarbageTypeDetailsScreen
@@ -31,6 +35,7 @@ fun MainNavigation(viewModel: MainViewModel, openGame: () -> Unit, navController
 
 
             LaunchedEffect(Unit, block = {
+                navController.popBackStack()
                 openGame()
             })
 
